17d7ff30a3b666d43bf635d3a72dcd2faa9bc7f7
Riyaz1203 <mohammedriyaz12032003@gmail.com>
2023-09-19 15:28:00 +0530

    Map pages

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -3,7 +3,14 @@ import styles1 from "@styles/index.module.scss";
 export default function MapComp() {
   return (
     <section className={styles1.section}>
-      <p>map page</p>
+      <p className="location-title">LOCATION INFO</p>
+
+      <img
+        className="map-image"
+        src="https://i.postimg.cc/7PGYFsSY/map-final-final.png"
+        alt=""
+        srcSet=""
+      />
     </section>
   );
 }
