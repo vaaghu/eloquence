c41201d64a076f620d66c679c064b13024643ed1
vaaghu <vignesh021102@gmail.com>
2023-09-21 15:02:32 +0530

    BUG,add info,event_info page

--- src/pages/event_Info.jsx
+++ src/pages/event_Info.jsx
@@ -5,7 +5,7 @@ import styles3 from "@styles/event.module.scss";
 
 import PropTypes from "prop-types";
 import arrowLeft from "@images/arrowLeft.svg";
-
+import contact from "@images/call_fill.svg";
 export default function EventInfoComp({ navigate }) {
   let eventInfo = JSON.parse(localStorage.getItem("eventInfo"));
   const [image, setImage] = useState(null);
@@ -35,8 +35,40 @@ export default function EventInfoComp({ navigate }) {
         <p>{eventInfo.title}</p>
       </div>
       <div>
-        <p>{eventInfo.description}</p>
+        <p className="description">{eventInfo.description}</p>
+        {eventInfo.notice && <p className="notice">{eventInfo.notice}</p>}
+        {eventInfo.rules && (
+          <div className="rules">
+            {eventInfo.rules.map((event, index) => {
+              return (
+                <div key={index}>
+                  <p className="rule_title">{Object.keys(event)[0]}:</p>
+                  <div>
+                    {event[Object.keys(event)[0]].map((element, index) => {
+                      return <p key={index}>{element}</p>;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
+      {eventInfo.contacts && (
+        <div>
+          for queries contact:
+          <br />
+          {eventInfo.contacts.map((person, index) => {
+            console.log(person);
+            return (
+              <a key={index} href={`tel:${person.number}`}>
+                <img src={contact} />
+                {person.name}
+              </a>
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 }
