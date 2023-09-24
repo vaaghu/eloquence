3d845015aae2c58b81664ff69d351b5c5e28ca12
vaaghu <vignesh021102@gmail.com>
2023-09-25 00:40:04 +0530

    home page done

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,43 @@ import React from "react";
 import link from "@images/link.svg";
 import call_fill from "@images/call_fill.svg";
 
-import styles1 from "@styles/index.module.scss";
+// import styles1 from "@styles/index.module.scss";
 import styles2 from "@styles/home.module.scss";
 export default function HomeComp() {
   return (
-    <section className={styles1.section}>
-      <p>home page</p>
+    <section className={styles2.section}>
+      <div className={styles2.topNav}>
+        <img src="https://i.postimg.cc/fR59c2pf/CAHCET-LOGO-FULL.png" alt="" />
+        <i>
+          C.ABDUL HAKEEM COLLEGE OF ENGINEERING AND TECHNOLOGY <br />{" "}
+          MELVISHARAM - 632 509
+        </i>
+        <img src="https://i.postimg.cc/BZ1HrCPJ/nba-logo.png" alt="" />
+      </div>
+      <div>
+        <img
+          className={styles2.mainImg}
+          src="https://i.postimg.cc/fL7HQtXH/mainpic.png"
+          alt=""
+        />
+      </div>
+      <div>
+        <h3>principal</h3>
+        <i className={styles2.para}>Dr.M. Sasi Kumar</i>
+      </div>
+      <div>
+        <h3>head of department</h3>
+        <i className={styles2.para}>Dr.K. Abrar Ahmed</i>
+      </div>
+      <div>
+        <h3>coordinators</h3>
+        <i className={styles2.para}>Dr.R.Z. Inamul Hussain</i>
+        <br />
+        <i className={styles2.para}>Mr.K.Suhail Ahmed</i>
+        <br />
+        <i className={styles2.para}>Mr.B.Palanisamy</i>
+        <br />
+      </div>
       <div>
         <h3>student coordinators</h3>
         <a
