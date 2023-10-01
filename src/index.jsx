a874459debd2a99ac64ec821dff79deb581ce496
vaaghu <vignesh021102@gmail.com>
2023-10-01 16:08:37 +0530

    BUG

--- src/index.jsx
+++ src/index.jsx
@@ -46,7 +46,7 @@ function IndexComp() {
 let root = null;
 root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div style={{ backgroundColor: "rgb(9, 21, 63)" }}>
+  <div style={{ backgroundColor: "rgb(9, 21, 63)", height: "101vh" }}>
     <Canva />
     <App />
   </div>
